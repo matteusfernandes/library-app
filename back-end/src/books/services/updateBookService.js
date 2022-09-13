e83e6d895e7311e51fbbf08fd1f4534a37b0f71c
Matteus Fernandes <matteus_gbi@hotmail.com>
2022-09-13 04:50:26 -0300

    feat: create a error handling function

---
+++ back-end/src/books/services/updateBookService.js
@@ -0,0 +1,8 @@
+const utils = require('../../utils');
+const { book } = require('../../database/models');
+
+module.exports = async (id, bookData) => {
+  const idBook = await book.findOne({ where: { id } });
+
+  if (!idBook) throw utils.error(404, 'livro não encontrado');
+};

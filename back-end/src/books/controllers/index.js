1d034b298ad15d81b158c77e654f81691c4aed14
Matteus Fernandes <matteus_gbi@hotmail.com>
2022-09-13 04:43:55 -0300

    feat: add update controller in index

--- back-end/src/books/controllers/index.js
+++ back-end/src/books/controllers/index.js
@@ -1,4 +1,4 @@
 module.exports.getAll = require('./getAllBooksController');
 module.exports.getBookById = require('./getBookByIdController');
 module.exports.createNewBook = require('./createNewBookController');
-
+module.exports.updateBook = require('./updateBookController');

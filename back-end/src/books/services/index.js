6dbedf0349bfcd4dc330b4ce911c89b8a6fe10e4
Matteus Fernandes <matteus_gbi@hotmail.com>
2022-09-13 04:54:18 -0300

    feat: create a update book service and add in index

--- back-end/src/books/services/index.js
+++ back-end/src/books/services/index.js
@@ -1,3 +1,4 @@
 module.exports.getAll = require('./getAllBooksService');
 module.exports.getBookById = require('./getBookByIdService');
 module.exports.createNewBook = require('./createNewBookService');
+module.exports.updateBook = require('./updateBookService');
--- back-end/src/books/services/updateBookService.js
+++ back-end/src/books/services/updateBookService.js
@@ -5,4 +5,8 @@ module.exports = async (id, bookData) => {
   const idBook = await book.findOne({ where: { id } });
 
   if (!idBook) throw utils.error(404, 'livro não encontrado');
+
+  const [updatedUser] = await book.update({...bookData }, { where: { id } });
+
+  return updatedUser;
 };

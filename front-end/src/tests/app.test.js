23e9b5b95b13fcf4d62d1df900218a28daebf492
Matteus Fernandes <matteus_gbi@hotmail.com>
2022-09-14 12:06:46 -0300

    test: create test to App.js

---
+++ front-end/src/tests/app.test.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testes do component <App.js />', () => {
+  test('Se na aplicação existe um link para a página Home', () => {
+    renderWithRouter(<App />);
+
+    const homeLink = screen.getByRole('link', { name: /home/i });
+    expect(homeLink).toBeInTheDocument();
+    userEvent.click(homeLink);
+    const homeAfterClick = screen.getByText(/Livros Encontrados/i);
+    expect(homeAfterClick).toBeInTheDocument();
+  });
+});

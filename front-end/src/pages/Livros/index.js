80514db8ec39ee1b31e08ed307f279bee7825a22
Matteus Fernandes <matteus_gbi@hotmail.com>
2022-09-14 21:06:42 -0300

    feat: create effect to fetch books in back-end

--- front-end/src/pages/Livros/index.js
+++ front-end/src/pages/Livros/index.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from '../../api';
 import Header from '../../components/header';
 
 function Livros() {
+  const [books, setBooks] = useState(null);
+
+  useEffect(() => {
+    api.get('/livros')
+      .then((apiResponse) => {
+        setBooks((apiResponse.data));
+      }).catch((err) => console.log(err));
+  }, []);
+
   return (
     <>
+      {console.log(books)}
       <Header />
       <main>
         <h1>Livros Encontrados</h1>
